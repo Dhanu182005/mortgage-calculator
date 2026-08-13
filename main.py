53f87ad87ddc2c70859c4c74583ce09f4fad0158
Dhanush <dhanushveligeti06@gmail.com>
2026-08-12 21:59:04 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from loan import Loan
-loan=Loan(100000, 5, 12)
-loan.calculate_payment()
+principal=float(input("Enter loan amount:"))
+interest=float(input("Enter annual interest rate(%):"))/100
+term=int(input("Enter loan term(years):"))
+loan=Loan(principal,interest,term,"years","monthly")
+print("\n-- Mortgage Results ---")
+print("Monthly Payment:",loan.monthly_payment)
+print("APR:",loan.apr,"%")
+print("APY:",loan.apy,"%")
